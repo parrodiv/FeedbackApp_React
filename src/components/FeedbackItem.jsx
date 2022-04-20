c03f42dfa2b057799e97e8a048dfa486e698be08
Alessandro Parrilla <paggio96@gmail.com>
2022-04-20 14:04:16 +0200

    Edit Feedback Event

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,11 @@
-import { FaTimes } from 'react-icons/fa';  //FaTimes è una x e il path react-icons/fa porta a font awesome
+import { FaTimes, FaEdit } from 'react-icons/fa';  //FaTimes è una x e il path react-icons/fa porta a font awesome
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
 import {useContext} from 'react'
 import  FeedbackContext  from '../context/FeedbackContext';
 
 function FeedbackItem({ item }) {
-  const {deleteFeedback} = useContext(FeedbackContext)
+  const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
 
   return (
     // possiamo passare una prop per impostare una condizione sullo stile: conditional class or conditional style, usiamo reverse che passo nel Card.jsx
@@ -15,6 +15,10 @@ function FeedbackItem({ item }) {
       {/* andiamo a prendere la prop handleDelete da FeedbackList che a sua volta gli viene passato da App.js e al parametro gli passiamo quello che ci serve. handleDelete contiene la funzione deleteFeedback  */}
       <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button className="edit">
+        <FaEdit onClick={() => editFeedback(item)} color="purple"/>
+        {/*passo come parametro l'item contenente id, text e rating del item su cui ho cliccato */}
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -3,15 +3,43 @@ import { v4 as uuidv4 } from 'uuid'; //genera id
 
 const FeedbackContext = createContext();
 
+//STATE1
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This is text from context',
+      text: 'This is feedback item 1',
       rating: 10,
     },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 7,
+    },
   ]);
+  //STATE2
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+    //al click del pencil settiamo il feedbackEdit con l'oggetto che contiene id, text e rating e l'inseriamo nel item che ora è un oggetto vuoto, dopo di che imposteremo edit in true
+  });
 
+  // Add feedback
+  const addFeedback = (newFeedback) => {
+    //aggiungo un id al nuovo object newFeedback
+    newFeedback.id = uuidv4();
+    //  const newArr = [newFeedback, ...feedback]
+    //con lo spread operator vengono copiati gli oggetti all'interno dell'array e incorporati al nuovo array
+    //sarebbe diverso da [newFeedback, feedback] darebbe [{newFeedback}, [feedback Array(n oggetti es (3))]]
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       //la funzione setFeedback va a modificare il contenuto di feedback
@@ -22,21 +50,21 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
-  const addFeedback = (newFeedback) => {
-    //aggiungo un id al nuovo object newFeedback
-    newFeedback.id = uuidv4();
-    //  const newArr = [newFeedback, ...feedback]
-    //con lo spread operator vengono copiati gli oggetti all'interno dell'array e incorporati al nuovo array
-    //sarebbe diverso da [newFeedback, feedback] darebbe [{newFeedback}, [feedback Array(n oggetti es (3))]]
-    setFeedback([newFeedback, ...feedback]);
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
   };
 
   return (
     <FeedbackContext.Provider
       value={{
-        feedback,  //feedback: feedback
+        feedback, //feedback: feedback
         deleteFeedback,
-        addFeedback
+        addFeedback,
+        editFeedback
       }}
     >
       {children}
